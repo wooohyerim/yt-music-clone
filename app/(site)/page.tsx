c0a6493c1d980559991b079978353c9057912937
wooohyerim <hyerim3414@gmail.com>
2024-04-03 12:31:07 +0900

    ADD : img, music, dummyData

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,10 @@
-// import { sleep } from "@/lib/utils";
+import { sleep } from "@/lib/utils";
 import React from "react";
 
-const page = () => {
+const page = async () => {
+  // console.log("before RootLayout sleep...");
+  // await sleep(4000);
+  // console.log("after RootLayout sleep...");
   // await sleep(2000); loading
 
   // throw new Error("error"); error
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/providers/themeProvider";
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
