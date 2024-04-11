94a1f4800d8bde8b8650c65202614e80db2d17fc
wooohyerim <hyerim3414@gmail.com>
2024-04-11 20:47:13 +0900

    ADD : carousel

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,6 +1,10 @@
 // import { sleep } from "@/lib/utils";
 import React from "react";
 import Category from "./components/Category";
+import PagePadding from "@/components/PagePadding";
+import PlayListCarousel from "@/components/PlayListCarousel";
+import { dummyPlaylistArray } from "@/lib/dummyData";
+import UserIcon from "@/components/UserIcon";
 
 const page = async () => {
   // console.log("before RootLayout sleep...");
@@ -9,11 +13,27 @@ const page = async () => {
   // await sleep(2000); loading
 
   // throw new Error("error"); error
+
+  const dummyPlaylistArray1 = [...dummyPlaylistArray];
+
   return (
-    <div className="min-h-[600px]">
-      <div className="mt-9"></div>
-      <Category />
-    </div>
+    <PagePadding>
+      <div className="min-h-[600px]">
+        <div className="mt-9"></div>
+        <Category />
+        <div className="mt-12"></div>
+        <PlayListCarousel
+          playListArray={[...dummyPlaylistArray1]}
+          Thumbnail={
+            <div className="w-[56px] h-[56px]">
+              <UserIcon size="lg" />
+            </div>
+          }
+          title="다시 듣기"
+          subTitle="woo"
+        />
+      </div>
+    </PagePadding>
   );
 };
 
---
+++ types/index.ts
@@ -0,0 +1,27 @@
+export interface Song {
+  name: string;
+  channelId: number;
+  channel: string;
+  src: string;
+  imageSrc: string;
+}
+
+export interface TopSong {
+  prevRank: number;
+  rank: number;
+}
+
+export interface PlayList {
+  id: number;
+  owner: string;
+  playlistName: string;
+  songList: Song[];
+}
+
+export interface Channel {
+  id: number;
+  subscribers: number;
+  name: string;
+  songList: Song[];
+  playlistArray: PlayList[];
+}
